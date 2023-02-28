4bae29b366152a3ebbfc84c480958e835032b3f5
Christoph Mewes <christoph@kubermatic.com>
2023-02-28 15:06:49 +0100

    fix boilerplate

--- pkg/agent/kubernetes/v2/types/record.go
+++ pkg/agent/kubernetes/v2/types/record.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2016 The Kubernetes Authors.
+Copyright 2021 The Telemetry Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -12,10 +12,6 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
-
-Telemetry takes the Record schema from Kubernetes spartakus project:
-https://github.com/kubernetes-retired/spartakus/blob/master/pkg/volunteer/kubernetes.go
-and some customized fields are added to fit Telemetry own use cases.
 */
 
 package types
